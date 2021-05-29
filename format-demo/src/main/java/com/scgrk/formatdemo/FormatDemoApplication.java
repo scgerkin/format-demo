0374a4c7691014f3e462e1bec437500374422356
Stephen Gerkin <stephen.gerkin@gmail.com>
2021-05-29 16:55:27 -0400

    Initial commit

---
+++ format-demo/src/main/java/com/scgrk/formatdemo/FormatDemoApplication.java
@@ -0,0 +1,13 @@
+package com.scgrk.formatdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormatDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormatDemoApplication.class, args);
+	}
+
+}
