03cde7b2541368988894bb7b24b0e4bfd20cfc82
unknown <stephen.gerkin@gmail.com>
2021-05-29 15:36:09 -0700

    update pom with new plugin

--- format-demo/src/main/java/com/scgrk/formatdemo/FormatDemoApplication.java
+++ format-demo/src/main/java/com/scgrk/formatdemo/FormatDemoApplication.java
@@ -3,11 +3,15 @@ package com.scgrk.formatdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+import java.util.Map;
+
 @SpringBootApplication
 public class FormatDemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FormatDemoApplication.class, args);
-    }
+public static void main(String[] args) {
 
+SpringApplication.run(FormatDemoApplication.class, args);
+}
 }
