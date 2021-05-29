f14b7157e3ee4a5e8de87e93ff936212dbca6e58
Stephen Gerkin <stephen.gerkin@gmail.com>
2021-05-29 18:43:43 -0400

    Testing pre-commit hook

--- format-demo/src/main/java/com/scgrk/formatdemo/FormatDemoApplication.java
+++ format-demo/src/main/java/com/scgrk/formatdemo/FormatDemoApplication.java
@@ -3,13 +3,11 @@ package com.scgrk.formatdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Map;
-
 @SpringBootApplication
 public class FormatDemoApplication {
 
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    SpringApplication.run(FormatDemoApplication.class, args);
-  }
+		SpringApplication.run(FormatDemoApplication.class, args);
+	}
 }
